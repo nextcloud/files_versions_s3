4ffad4863e1727a57caba3291f8cac9b8ca4fd90
Robin Appelman <robin@icewind.nl>
2023-06-01 21:49:39 +0200

    cs fix
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,15 +1,18 @@
 <?php
-$finder = PhpCsFixer\Finder::create()
-	->exclude('vendor')
-	->in(__DIR__)
-;
-return PhpCsFixer\Config::create()
-	->setRules([
-		'@PSR2' => true,
-		'array_syntax' => ['syntax' => 'short'],
-		'braces' => ['position_after_functions_and_oop_constructs' => 'same'],
-		'binary_operator_spaces' => ['align_double_arrow' => true, 'align_equals' => false],
-	])
-	->setIndent("\t")
-	->setFinder($finder)
-	;
+
+declare(strict_types=1);
+
+require_once './vendor/autoload.php';
+
+use Nextcloud\CodingStandard\Config;
+
+$config = new Config();
+$config
+	->getFinder()
+	->ignoreVCSIgnored(true)
+	->notPath('build')
+	->notPath('l10n')
+	->notPath('src')
+	->notPath('vendor')
+	->in(__DIR__);
+return $config;
